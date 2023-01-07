07cd6354e7d0258607dc8d3c5b146be64e594d46
Pukiediederik <diederik.boeren@gmail.com>
2023-01-07 22:25:07 +0000

    Implemented comparison operators for vectors

--- include/vector.hpp
+++ include/vector.hpp
@@ -389,17 +389,24 @@ namespace ft
 
 	// Operators
 	template<class T, class Alloc>
-	bool operator==(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs);
+	bool operator==(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs)
+	{ return lhs.size() == rhs.size() && ft::equal(lhs.begin(), lhs.end(), rhs.begin()); }
 	template<class T, class Alloc>
-	bool operator!=(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs);
+	bool operator!=(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs)
+	{ return !(lhs == rhs); }
+
 	template<class T, class Alloc>
-	bool operator<(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs);
+	bool operator<(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs)
+	{ return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()); }
 	template<class T, class Alloc>
-	bool operator>(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs);
+	bool operator<=(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs)
+	{ return !(rhs < lhs); }
 	template<class T, class Alloc>
-	bool operator<=(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs);
+	bool operator>(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs)
+	{ return rhs < lhs; }
 	template<class T, class Alloc>
-	bool operator>=(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs);
+	bool operator>=(const ft::vector<T, Alloc>& lhs, const ft::vector<T, Alloc>& rhs)
+	{ return !(lhs < rhs); }
 
 	template<class T, class Alloc>
 	void swap(ft::vector<T, Alloc>& x, ft::vector<T, Alloc>& y);
