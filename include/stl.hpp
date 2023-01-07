5a4b09a5809f3cbe03152b096a70c1be9eda3b60
Pukiediederik <diederik.boeren@gmail.com>
2023-01-07 21:54:59 +0000

    Implemented equal and lexicographical_compare

--- include/stl.hpp
+++ include/stl.hpp
@@ -45,6 +45,54 @@ struct is_integral<const T> : public is_integral<typename remove_const<T>::type>
 	struct enable_if { };
 	template <class T>
 	struct enable_if<true, T> { typedef T type; };
+
+	// lexicographical_compare
+	template <class InputIter1, class InputIter2>
+	bool lexicographical_compare (InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2)
+	{
+		for(; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
+		{
+			if (*first1 < *first2)
+				return true;
+			if (*first2 < *first1)
+				return false;
+		}
+		return (first1 == last1) && (first2 != last2);
+	}
+	template <class InputIter1, class InputIter2, class Compare>
+	bool lexicographical_compare (InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, Compare comp)
+	{
+		for(; (first1 != last1) && (first2 != last2); ++first1, ++first2)
+		{
+			if (comp(*first1, *first2))
+				return true;
+			if (comp(*first2, *first1))
+				return false;
+		}
+		return (first1 == last1) && (first2 != last2);
+	}
+
+	// equal
+	template<class InputIt1, class InputIt2>
+	bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2)
+	{
+		for (; first1 != last1; ++first1, ++first2)
+		{
+			if (!(*first1 == *first2))
+				return false;
+		}
+		return true;
+	}
+	template<class InputIt1, class InputIt2, class BinaryPredicate>
+	bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p)
+	{
+		for (; first1 != last1; ++first1, ++first2)
+		{
+			if (!p(*first1, *first2))
+				return false;
+		}
+		return true;
+	}
 }
 
 #endif
