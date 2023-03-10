7f725b7f48c8faf7315828de3409038ab0c3b24e
Pukiediederik <diederik.boeren@gmail.com>
2023-03-10 17:46:35 +0000

    Added docs for stack

--- include/stack.hpp
+++ include/stack.hpp
@@ -22,20 +22,18 @@ namespace ft
 		explicit stack(const Container& cont = Container()) : c(cont) { }
 		stack(const stack& copy) :c(copy.c) { }
 		~stack () { }
-		stack& operator=(const stack& copy)
-		{
-			c = copy.c;
-			return *this;
-		}
+		stack& operator=(const stack& copy) { c = copy.c; return *this; }
 
-		// Functions
+		// Accessors
 		const_reference top() const { return c.back(); }
 		bool empty() const { return c.empty(); }
 		size_type size() const { return c.size(); }
 
+		// Modification
 		void push(const value_type& value) { c.push_back(value); }
 		void pop() { c.pop_back(); }
 
+		// Need to make these friends so they can access 'c'
 		template <typename A,typename B>
 		friend bool operator==(const ft::stack<A, B> lhs, const ft::stack<A, B> rhs);
 		template <typename A,typename B>
@@ -51,6 +49,7 @@ namespace ft
 
 	};
 
+	// Comparison operators
 	template <typename T, typename Container>
 	bool operator==(const ft::stack<T, Container> lhs, const ft::stack<T, Container> rhs)
 	{ return lhs.c == rhs.c; }
